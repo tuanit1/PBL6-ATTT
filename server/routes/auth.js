21f148aa23a4930df0db5247aad88d0661e161bb
goprodeptra01 <tapnguyen01@gmail.com>
2022-12-02 19:13:10 +0700

    JWT authentication

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controller/auth')
+const verifyToken = require('../middleware/auth')
 
 // @route Post api/user
 // @desc Create participant
@@ -8,5 +9,5 @@ router.post('/login/:user_id', authController.login)
 
 router.post('/token', authController.rfToken)
 
-router.put('/logout', authController.logout)
+router.put('/logout', verifyToken, authController.logout)
 module.exports = router
